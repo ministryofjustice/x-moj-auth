708d00b39cce72b7274b18dbb519909d463963c9
Rob McKinnon <@digital.justice.gov.uk>
2013-11-26 17:05:44 +0000

    rename to HTTP_SECURE_TOKEN

--- lib/rack_moj_auth/resources.rb
+++ lib/rack_moj_auth/resources.rb
@@ -1,7 +1,7 @@
 module RackMojAuth
   class Resources
 
-    SECURE_TOKEN = 'HTTP_X_SECURE_TOKEN'
+    SECURE_TOKEN = 'HTTP_SECURE_TOKEN'
     ROLES = 'X-USER-ROLES'
     USER_ID = 'X-USER-ID'
   end
